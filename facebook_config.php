83e1a7dfaf3f5ff511332c57f78b5ed43a867aa8
Michael <darundaymichael4@gmail.com>
2024-06-14 00:08:28 +0800

    dfgdfgd

--- facebook_config.php
+++ facebook_config.php
@@ -1,7 +1,7 @@
 <?php
 // Update the following variables
-$facebook_oauth_app_id = '367110759697321';
-$facebook_oauth_app_secret = '2b32a1c8a28b1fa5bcf9679288a191b8';
+$facebook_oauth_app_id = '';
+$facebook_oauth_app_secret = '';
 // Must be the direct URL to the facebook-oauth.php file
-$facebook_oauth_redirect_uri = 'http://localhost/ipt101(lab5)/facebook_login.php';
+$facebook_oauth_redirect_uri = 'http://localhost/ipt101(lab5)/facebook_login.php'; //sample
 $facebook_oauth_version = 'v20.0';
--- google_config.php
+++ google_config.php
@@ -6,14 +6,14 @@ require_once 'vendor/autoload.php';
 $gClient = new Google_Client();
 
 // Set up Google project credentials 
-$gClient->setClientId("535111942707-ac8kddin2o20mefs3un3legkhfehdprv.apps.googleusercontent.com");  // Google Client ID from the Google Developer Console
-$gClient->setClientSecret("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");   // Your Google Client Secret from the Developer Console
+$gClient->setClientId("");  // Google Client ID from the Google Developer Console
+$gClient->setClientSecret("");   // Your Google Client Secret from the Developer Console
 
 // App name
 $gClient->setApplicationName("IPT LAB5");
 
 // Set the URL where Google should redirect the user after authorization
-$gClient->setRedirectUri("http://localhost/ipt101(lab5)/google_login.php");
+$gClient->setRedirectUri("http://localhost/ipt101(lab5)/google_login.php"); //sample
 
 // Specify what information to access from the user's Google account
 $gClient->addScope("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
